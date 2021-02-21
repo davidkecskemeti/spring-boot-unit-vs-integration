dbf57da4ad5e0c4fd38f73d8ecf1ebaabfd80724
davidkecskemeti <dxdy421@gmail.com>
2021-02-21 20:09:46 +0000

    Initial commit

---
+++ src/main/java/com/dk/testing/unitvsintegration/SpringBootUnitVsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dk.testing.unitvsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitVsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitVsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/testing/unitvsintegration/SpringBootUnitVsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.testing.unitvsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitVsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
